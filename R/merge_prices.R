9b8d8a1717e73cf84cd1b9aa3e46ef9c4a42cde8
Marianna Rottoli <rottoli@pik-potsdam.de>
2019-04-25 15:46:32 +0200

    updated merge_prices function: iso level and compatible with current model status

--- R/merge_prices.R
+++ R/merge_prices.R
@@ -9,7 +9,7 @@
 #' @importFrom rmndt toISO_dt toRegions_dt
 #' @export
 
-merge_prices <- function(gdx, REMINDmapping, path2intensities, path2UCD) {
+merge_prices <- function(gdx, REMINDmapping, path2intensities, path2nonfuel) {
     ## report prices from REMIND gdx in 2005$/MJ
     tdptwyr2dpgj <- 31.71  #TerraDollar per TWyear to Dollar per GJ
 
@@ -41,11 +41,6 @@ merge_prices <- function(gdx, REMINDmapping, path2intensities, path2UCD) {
 
     fuel_price_REMIND <- toISO_dt(tmp, mapping = REMINDmapping)
 
-    fuel_price_REMIND <- toRegions_dt(fuel_price_REMIND, GCAM2ISO_MAPPING,
-                                   datacol = "sector_fuel",
-                                   valuecol = "value",
-                                   strategy = "gdp")
-
     fuel_price_REMIND[, `:=`(sector_fuel, ifelse(grepl("Electricity", sector_fuel),
                                                  "elect_td_trn", sector_fuel))]
     fuel_price_REMIND[, `:=`(sector_fuel, ifelse(grepl("Hydrogen", sector_fuel),
@@ -62,39 +57,39 @@ merge_prices <- function(gdx, REMINDmapping, path2intensities, path2UCD) {
 
     ## fuel price in 2005USD/GJ -> 1990USD/EJ
 
-    fuel_price_REMIND[, fuel_price_EJ := fuel_price * CONV_2005USD_1990USD * 1e9]
+    fuel_price_REMIND[, fuel_price := fuel_price * CONV_2005USD_1990USD * 1e9]
 
     ## apply the markup fro NG and coal (coal can be negative!!, as a workaround I
     ## make it positive and apply the markup):
-    fuel_price_REMIND[, fuel_price_EJ := ifelse(sector_fuel == "delivered gas", fuel_price_EJ/0.2,
-                                                fuel_price_EJ)]
-    fuel_price_REMIND[, fuel_price_EJ := ifelse(sector_fuel == "delivered coal",
-                                                abs(fuel_price_EJ/0.2), fuel_price_EJ)]
+    fuel_price_REMIND[, fuel_price := ifelse(sector_fuel == "delivered gas", fuel_price/0.2,
+                                                fuel_price)]
+    fuel_price_REMIND[, fuel_price := ifelse(sector_fuel == "delivered coal",
+                                                abs(fuel_price/0.2), fuel_price)]
 
-    if(all(fuel_price_REMIND[year == 1990]$fuel_price_EJ == 0)){
+    if(all(fuel_price_REMIND[year == 1990]$fuel_price == 0)){
         ## if no 1990 prices are found, lets use 2005 prices and issue warning
         warning("No 1990 fuel prices found in REMIND, using 2005 prices.")
-        fuel_price_REMIND[year == 1990, fuel_price_EJ := fuel_price_REMIND[year==2005]$fuel_price_EJ]
+        fuel_price_REMIND[year == 1990, fuel_price := fuel_price_REMIND[year==2005]$fuel_price]
     }
 
-    stopifnot(all(fuel_price_REMIND$fuel_price_EJ > 0))
+    stopifnot(all(fuel_price_REMIND$fuel_price > 0))
 
     ## join with vehicle intensity and load factor to get the 1990USD/pkm
 
     km_intensity <- readRDS(path2intensities)
     km_intensity <- km_intensity[year %in% years,]
-    fuel_price_REMIND <- merge(fuel_price_REMIND, km_intensity, by = c("region",
+    fuel_price_REMIND <- merge(fuel_price_REMIND, km_intensity, by = c("iso",
         "year", "sector_fuel"), all.y = TRUE)
 
 
     ## fuel_price [$/EJ * EJ/Mpkm * Mpkm/pkm],
-    tech_cost2 <- fuel_price_REMIND[, fuel_price_pkm := fuel_price_EJ * EJ_Mpkm_final * 1e-6]
+    tech_cost2 <- fuel_price_REMIND[, fuel_price_pkm := fuel_price * EJ_Mpkm_final * 1e-6]
     tech_cost2=tech_cost2[,-c("EJ_Mpkm","EJ_Mpkm_adjusted","lambda","EJ_Mpkm")]
 
     ## merge the non energy prices, they are $/pkm
-    non_energy_cost_result <- readRDS(path2UCD)[["non_energy_cost"]]
+    non_energy_cost_result <- readRDS(path2nonfuel)
     tech_cost2 <- merge(tech_cost2, non_energy_cost_result,
-                        by = c("region", "year", "technology",
+                        by = c("iso", "year", "technology",
                                "vehicle_type", "subsector_L1",
                                "subsector_L2", "subsector_L3","sector"),
                         all.x = TRUE)
@@ -105,10 +100,7 @@ merge_prices <- function(gdx, REMINDmapping, path2intensities, path2UCD) {
     tech_cost2 <- tech_cost2[, non_fuel_price := ifelse(technology %in% c("Coal",
         "Adv-Electric", "Adv-Liquid") & vehicle_type == "Freight Rail_tmp_vehicletype",
         non_fuel_price[technology == "Liquids" & vehicle_type == "Freight Rail_tmp_vehicletype"],
-        non_fuel_price), by = c("region", "year")]
-
-    ## delete the MiniCars from EU-12 (they should not be in the load factor...)
-    ## tech_cost2 <- tech_cost2[!(region == "EU-12" & vehicle_type == "Mini Car"), ]
+        non_fuel_price), by = c("iso", "year")]
 
     ## calculate the total price
     tech_cost2[, tot_price := fuel_price_pkm + non_fuel_price]
