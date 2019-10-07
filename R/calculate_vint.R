136014fdfee76594e383eecb4bd4036768ae21f5
Marianna Rottoli <rottoli@pik-potsdam.de>
2019-10-07 18:13:33 +0200

    Vintages are calculated every year. Finer grain representation 2010 fleet.

--- R/calculate_vint.R
+++ R/calculate_vint.R
@@ -8,8 +8,38 @@
 
 calcVint <- function(shares, totdem_regr, prices, mj_km_data, years){
 
-  ## vintages prefer all equal time steps
-  tall = seq(2010, 2150,5)
+  ## function that allows to find the depreciation for the already partially depreciated capacity (e.g. a 5 years old car)
+  find2010depreciation = function(Ddt, val_ref){
+    Ddt_2010x = Ddt[index_yearly>= which.min(abs(Ddt[,D] -                 ## depreciation already started at some point in the past: we are not anymore at index 0
+                                                   val_ref))]            ## to find the index, I have to look for the closest value D in the table, to then find the index at which it belongs
+    Ddt_2010x = Ddt_2010x[,index := seq(1,length(index_yearly),1)]  ## reset so that 1st index is named 1
+    Ddt_2010x[, index_yearly:= NULL]                                         ## remove yearly index
+    Ddt_2010x=rbind(data.table(index = c(0), D = 1), Ddt_2010x)              ## add starting value
+    return(Ddt_2010x)
+  }
+
+  ## function that allows to calculate , given the depreciation trend for the capacity built before 2010
+  find2010cap = function(percentage, dem, cap, Ddt){
+    Cap_2010x = copy(dem)
+    Cap_2010x = Cap_2010x[, totdem := percentage*totdem]
+    setnames(Cap_2010x, old = "totdem", new = "C_2010x")
+    ## apply the starting value to all time steps
+    Cap_2010x[, C_2010x := C_2010x[year == baseyear], by = c("iso", "subsector_L1", "sector")]
+    ## apply depreciation and clean up data
+    Cap_2010x = merge(Cap_2010x, Ddt_2010_new[, year:= tall], by = "year")
+    Cap_2010x = Cap_2010x[, C_2010x := C_2010x*D]
+    Cap_2010x = Cap_2010x[, c("iso", "sector", "subsector_L1", "year", "C_2010x")]
+    Cap_2010x = merge(Cap_2010x, Ddt_2010_new[, year:= tall], by = "year")
+    Cap_2010x = Cap_2010x[, C_2010x := C_2010x*D]
+    Cap_2010x[, c("index", "D"):= NULL]
+    setnames(Cap_2010x, old = "C_2010x", new =paste0("C_2010", cap))
+    return(Cap_2010x)
+  }
+
+
+
+  ## vintages all equal time steps, equal to 1 year
+  tall = seq(2010, 2100,1)
   ## last historical year
   baseyear = 2010
 
@@ -26,7 +56,6 @@ calcVint <- function(shares, totdem_regr, prices, mj_km_data, years){
   shares_4W = merge(shares_4W, S1S2, by = c("iso", "year", "subsector_L2"))
   shares_4W[, share := share*shareS1S2]
   shares_4W = shares_4W[subsector_L1 == "trn_pass_road_LDV_4W", c("iso", "year", "subsector_L1", "share")]
-
   ## find total demand LDV
   passdem = totdem_regr[sector == "trn_pass"]
   passdem = merge(passdem, shares_4W, by = c("iso", "year"))
@@ -37,33 +66,46 @@ calcVint <- function(shares, totdem_regr, prices, mj_km_data, years){
                       extrapolate=T)
   setnames(passdem, old = "value", new = "totdem")                      ## rename back column
 
-  ## depreciation
-  paux = 15   ## approximate lifetime of a car
-  Ddt = data.table(index_yearly = seq(1,140,1))
+  paux = 20   ## approximate lifetime of a car
+  Ddt = data.table(index_yearly = seq(1,length(tall)-1,1))
   Ddt[, D := 1-((index_yearly-0.5)/paux)^4]
   Ddt[, D := ifelse(D<0,0,D)]
 
   ## first time step has no depreciation
   Ddt = rbind(data.table(index_yearly = 0, D = 1), Ddt)
 
-  ## five years time steps: only what happens every 5 years matters
-  Ddt = Ddt[index_yearly %in% index_yearly[index_yearly %% 5 == 0]]   ## select index_yearly such that it is multiple of 5 years
-  Ddt[, index := seq(0,length(index_yearly)-1, 1)]   ## index is on a 5 years basis
-  Ddt[, index_yearly:= NULL]
+  ## to find average depreciated historical capacity, I have to identify the reference depreciation state of 3 groups:
+  value_new = Ddt[index_yearly == 0, D]                                ## new vehicles (less then 1 year)
+  value_old10 = Ddt[index_yearly == 10, D]                               ## around 10 years old vehicles
+  value_old15 = Ddt[index_yearly == 15, D]                               ## around 15 years old vehicles
+  value_mean = mean(Ddt[index_yearly>0 & index_yearly<10 ,D])            ## all other vehicles (between 1.1 and 9.9 years)
 
-  ## find historical capacity vintaging in time
-  Cap_2010 = copy(passdem)
-  setnames(Cap_2010, old = "totdem", new = "C_2010")
-  ## apply the starting value to all time steps
-  Cap_2010[, C_2010 := 1/3*C_2010[year == baseyear], by = c("iso", "subsector_L1", "sector")] ## approximate: 1/3 of the cars in 2010 are new enough to be vintages later. Others are phased out
+  ## the depreciation in time for the corresponding categories is given by:
+  Ddt_2010_new = find2010depreciation(Ddt, value_new)
+  Ddt_2010_old10 = find2010depreciation(Ddt, value_old10)
+  Ddt_2010_old15 = find2010depreciation(Ddt, value_old15)
+  Ddt_2010_mean = find2010depreciation(Ddt, value_mean)
 
-  ## apply depreciation and clean up data
-  Cap_2010 = merge(Cap_2010, Ddt[, year:= tall], by = "year")
-  Cap_2010 = Cap_2010[, C_2010 := C_2010*D]
-  Cap_2010 = Cap_2010[,c("iso", "sector", "subsector_L1", "year", "C_2010")]
+  ## the historical capacity from each group is found. Data from https://www.acea.be/uploads/statistic_documents/ACEA_Report_Vehicles_in_use-Europe_2018.pdf
+  Cap_2010new = find2010cap(0.05, dem = passdem,"new", Ddt_2010_new[, year:= tall])
+  Cap_2010old10 = find2010cap(0.1, dem = passdem, "old10", Ddt_2010_old10[, year:= tall[1:nrow(Ddt_2010_old10)]])
+  Cap_2010old15 = find2010cap(0.2, dem = passdem, "old15", Ddt_2010_old15[, year:= tall[1:nrow(Ddt_2010_old15)]])
+  Cap_2010mean = find2010cap(0.65, dem = passdem, "mean", Ddt_2010_mean[, year:= tall[1:nrow(Ddt_2010_mean)]])
+
+  ## find the total historical depreciation
+  Cap_2010 <- list(Cap_2010new, Cap_2010old10, Cap_2010old15, Cap_2010mean)
+  keys=c("iso","year","sector","subsector_L1")
+  Cap_2010 = lapply(Cap_2010, function(i) setkeyv(i, keys))
+  Cap_2010 <- Reduce(function(...) merge(..., all = T), Cap_2010)
+  Cap_2010[, C_2010 := C_2010new + C_2010old10 + C_2010old15 + C_2010mean][, c("C_2010new", "C_2010mean","C_2010old15", "C_2010old10") := NULL]
+
   ## create vintages of base year capacity
   Vint = Cap_2010[year > baseyear]
   Vint[year == tall[tall>baseyear][1], vint := C_2010]
+
+  ## five years time steps: only what happens every 5 years matters
+  setnames(Ddt, old = "index_yearly", new = "index")
+
   ## create tmp structure that is used in the for loop
   ##NB this is NOT goingto work when there are trucks! they belong to a different sector!
   tmp = CJ(year = tall, iso = unique(Cap_2010$iso), subsector_L1 = unique(Cap_2010$subsector_L1), sector = unique(Cap_2010$sector))
@@ -136,6 +178,10 @@ calcVint <- function(shares, totdem_regr, prices, mj_km_data, years){
   ## find the average composition of the new additions+vintages
   FV_4W = merge(vintcomp, newcomp, by = c("iso", "subsector_L1", "year", "totdem", "vehicle_type", "technology", "sector"))
   FV_4W = FV_4W[,.(shareFV = sum(sharetech_vint, sharetech_new)), by = c("iso", "year", "subsector_L1", "vehicle_type", "technology")]
+  ## extend to 2110 2130 2150 using 2100 value
+  FV_4W2100 = FV_4W[year == 2100]
+  FV_4W = rbind(FV_4W, FV_4W2100[, year := 2110], FV_4W2100[, year := 2130], FV_4W2100[, year := 2150])
+
 
   ## updated values of FV_shares
   shares$FV_shares = merge(FV[(subsector_L1 != "trn_pass_road_LDV_4W")| (subsector_L1 == "trn_pass_road_LDV_4W" & year == 2010)],
@@ -170,6 +216,9 @@ calcVint <- function(shares, totdem_regr, prices, mj_km_data, years){
   totcost = totcost[, totcost := sum(sharevint*non_fuel_price_vint +(1-sharevint)*non_fuel_price), by = c("iso", "year", "subsector_L1", "technology", "vehicle_type", "sector")]
   ## calculate new tot cost as sum of fuel cost and non fuel cost
   totcost[, tot_price := fuel_price_pkm + non_fuel_price]
+  ## extend to 2110 2130 2150 using 2100 value
+  totcost2100 = totcost[year == 2100]
+  totcost = rbind(totcost, totcost2100[, year := 2110], totcost2100[, year := 2130], totcost2100[, year := 2150])
   ## updated values of FV_shares
   prices$base = merge(prices$base[(subsector_L1 != "trn_pass_road_LDV_4W")| (subsector_L1 == "trn_pass_road_LDV_4W" & year %in% c(1990, 2005,2010))],
                 totcost[year %in% years[years>baseyear], c("iso", "technology", "year", "vehicle_type", "subsector_L1", "subsector_L2", "subsector_L3", "sector", "non_fuel_price", "tot_price", "fuel_price", "fuel_price_pkm", "EJ_Mpkm_final", "tot_VOT_price", "sector_fuel")], by = names(prices$base), all = TRUE)
@@ -202,6 +251,9 @@ calcVint <- function(shares, totdem_regr, prices, mj_km_data, years){
   totint[, sharevint := ifelse(year <=2005 & is.na(sharevint), 0, sharevint)]
   ## weighted average to find the total cost of the LDV fleet
   totint = totint[, totint := sum(sharevint*MJ_km_vint +(1-sharevint)*MJ_km), by = c("iso", "year", "subsector_L1", "technology", "vehicle_type", "sector")]
+  ## extend to 2110 2130 2150 using 2100 value
+  totint2100 = totint[year == 2100]
+  totint = rbind(totint, totint2100[, year := 2110], totint2100[, year := 2130], totint2100[, year := 2150])
   ## updated values of FV_shares
   mj_km_data = merge(mj_km_data[(subsector_L1 != "trn_pass_road_LDV_4W")| (subsector_L1 == "trn_pass_road_LDV_4W" & year %in% c(1990, 2005,2010))],
                       totint[year %in% years[years>baseyear], c("iso", "technology", "year", "vehicle_type", "subsector_L1", "subsector_L2", "subsector_L3", "sector", "sector_fuel", "MJ_km")], by = names(mj_km_data), all = TRUE)
@@ -209,6 +261,7 @@ calcVint <- function(shares, totdem_regr, prices, mj_km_data, years){
   return(list(prices = prices,
               shares = shares,
               mj_km_data = mj_km_data,
-              vint_composition = vintcomp_startyear))
+              vintcomp = vintcomp[year %in% years],
+              newcomp = newcomp[year %in% years]))
 
 }
