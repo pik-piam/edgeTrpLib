f6210acfa16617fc9deb1b581e0ee8cb90a4343d
Marianna Rottoli <rottoli@pik-potsdam.de>
2020-03-20 16:55:50 +0100

    Learning for BEVs is based on the value before rebates.

--- R/learningFunctions.R
+++ R/learningFunctions.R
@@ -9,7 +9,7 @@
 #' @import data.table
 #' @export
 
-applylearning <- function(non_fuel_costs, gdx,REMINDmapping,EDGE2teESmap, demand_BEVtmp, ES_demandpr){
+applylearning <- function(non_fuel_costs, gdx,REMINDmapping,EDGE2teESmap, demand_BEVtmp, ES_demandpr, rebates_febates){
   ## find the estimated number of cars
   demand = merge(ES_demand, ES_demandpr)
   demand = demand[, ratio := demand/demandpr][,-c("demand", "demandpr")] ## ratio from previous iteration of total demand
@@ -31,14 +31,21 @@ applylearning <- function(non_fuel_costs, gdx,REMINDmapping,EDGE2teESmap, demand
 
   ## only BEV car costs are affected
   nonfuel_costsBEV = nonfuel_costs[technology =="BEV" & subsector_L1 == "trn_pass_road_LDV_4W",]
-  nonfuel_costsBEV[year >= 2020, non_fuel_price := non_fuel_price[year == 2020], by = c("iso", "vehicle_type")]
+
+  if (rebates_febates) {
+    ## after 2035, the "original" price of 2020 (before the rebates) is used
+    nonfuel_costsBEV[, non_fuel_price := ifelse(year > 2035, non_fuel_price[year == 2020]), by = c("iso", "vehicle_type")]
+  } else {
+    ## in case of no rebates, the price of 2020 applies to all time steps
+    nonfuel_costsBEV[year >= 2020, non_fuel_price := non_fuel_price[year == 2020], by = c("iso", "vehicle_type")]
+  }
+
   nonfuel_costsBEV = merge(demand, nonfuel_costsBEV, all.x = TRUE, by = "year")
   ## powertrain represents ~20% of the total purchase price, which represents the ~80% of the non-fuel price
   batterycomponent = 0.2*0.8 ## average number 80% for purchase cost
   nonfuel_costsBEV[year >= 2020, non_fuel_price := ifelse(!is.na(factor),factor*batterycomponent*non_fuel_price+(1-batterycomponent)*non_fuel_price, non_fuel_price)]
   nonfuel_costsBEV[,c("factor", "cumul", "vehicles_number"):= NULL]
-  nonfuel_costsBEV = nonfuel_costsBEV[year >= 2020,]
-  nonfuel_costs = nonfuel_costs[!(technology=="BEV" & subsector_L1 =="trn_pass_road_LDV_4W" & year >= 2020),]
+  nonfuel_costs = nonfuel_costs[!(technology=="BEV" & subsector_L1 =="trn_pass_road_LDV_4W" & year >=2020),]
   nonfuel_costs = rbind(nonfuel_costs, nonfuel_costsBEV)
 
   return(nonfuel_costs)
@@ -77,6 +84,7 @@ calc_num_vehicles_stations <- function(norm_dem, ES_demand_all){
 
   stations[, fracst := statnum/sum(statnum), by = c("iso", "year")]
   stations = stations[technology %in% c("BEV", "NG", "FCEV"),]
+
   stations = approx_dt(stations, seq(2020, 2101, 1),
                        xcol = "year", ycol = "fracst",
                        idxcols = c("iso", "technology"),
