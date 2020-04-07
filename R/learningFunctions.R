60fc6fc63c1cd63ca17f82f905b254d07723095a
Marianna Rottoli <marianna.rottoli@mail.polimi.it>
2020-04-07 14:08:17 +0200

    Learning for FCEVs and BEVs fixes.

--- R/learningFunctions.R
+++ R/learningFunctions.R
@@ -9,49 +9,72 @@
 #' @import data.table
 #' @export
 
-applylearning <- function(non_fuel_costs, gdx,REMINDmapping,EDGE2teESmap, demand_BEVtmp, ES_demandpr, rebates_febates){
+applylearning <- function(non_fuel_costs, gdx,REMINDmapping,EDGE2teESmap, demand_learntmp, ES_demandpr, rebates_febatesBEV, rebates_febatesFCEV){
   ## find the estimated number of cars
   demand = merge(ES_demand, ES_demandpr)
   demand = demand[, ratio := demand/demandpr][,-c("demand", "demandpr")] ## ratio from previous iteration of total demand
-  demand = merge(demand, demand_BEVtmp, all.x =TRUE, by = c("iso", "year"))
+  demand = merge(demand, demand_learntmp, all.x =TRUE, by = c("iso", "year"))
   demand[, vehicles_number := vehicles_number*ratio]
 
   ## find the cumulative capacity and the reduction factor of costs
-  demand = demand[,.(vehicles_number = sum(vehicles_number)), by = "year"]
+  demand = demand[,.(vehicles_number = sum(vehicles_number)), by = c("technology", "year")]
   demand = demand[!is.nan(vehicles_number) & !is.na(vehicles_number)]
-  demand[, cumul := cumsum(vehicles_number)]
-  initialcap = min(demand$cumul[demand$cumul != 0 & !is.na(demand$cumul)])
-  initialyear = demand[demand$cumul == initialcap,year]
-  demand[, cumul := cumul/initialcap]
-  demand[, cumul := ifelse(year>2100, cumul[year == 2100], cumul)]
-  demand[, cumul := ifelse(year<initialyear, cumul[year == initialyear], cumul)]
-  demand[, factor := cumul^-2.5]  ## according to https://about.bnef.com/blog/behind-scenes-take-lithium-ion-battery-prices/  LR ~ 18% -> b=ln(LR)/ln(2)~-2.5
-                                  ## same for https://www.sciencedirect.com/science/article/pii/S0959652618337211 (LR = 23%)
+  demand[, cumul := cumsum(vehicles_number), by = "technology"]
+  initialcap = demand[cumul!=0 & !is.na(demand$cumul),]
+  initialcap = initialcap[ , .SD[which.min(cumul)], by = "technology"]
+  initialcap = initialcap[,.(technology, initialcap = cumul, initialyear = year)]
+  demand = merge(demand, initialcap, by = c("technology"))
+  demand[, cumul := cumul/initialcap, by = "technology"]
+  demand[, cumul := ifelse(year>2100, cumul[year == 2100], cumul), by = "technology"]
+  demand[, cumul := ifelse(year<=initialyear, cumul[year == initialyear], cumul), by = "technology"]
+
+  exponent = data.table(b = c(-2.5, -2.5), technology =  c("BEV", "FCEV")) ## according to https://about.bnef.com/blog/behind-scenes-take-lithium-ion-battery-prices/  LR ~ 18% -> b=ln(LR)/ln(2)~-2.5
+                  ## same for https://www.sciencedirect.com/science/article/pii/S0959652618337211 (LR = 23%)
+  ## for FCEVs: https://www.sciencedirect.com/science/article/abs/pii/S0360544218302998
+
+  demand = merge(exponent, demand, by = "technology")
+  demand[, factor := cumul^b]
+  demand[, b := NULL]
   demand[, factor := ifelse(is.infinite(factor), 1, factor)] ## factor is 1 if there is no increase in cumulated demand (->no decrease in costs)
 
   ## only BEV car costs are affected
-  nonfuel_costsBEV = nonfuel_costs[technology =="BEV" & subsector_L1 == "trn_pass_road_LDV_4W",]
+  nonfuel_costslearn = nonfuel_costs[technology %in% c("BEV", "FCEV") & subsector_L1 == "trn_pass_road_LDV_4W",]
 
-  if (rebates_febates) {
+
+  if (rebates_febatesBEV) {
     ## after 2035, the "original" price of 2020 (before the rebates) is used
-    nonfuel_costsBEV[, non_fuel_price := ifelse(year > 2035, non_fuel_price[year == 2020], non_fuel_price), by = c("iso", "vehicle_type")]
+    nonfuel_costslearn[technology == "BEV", non_fuel_price := ifelse(year > 2035, non_fuel_price[year == 2020], non_fuel_price), by = c("iso", "vehicle_type", "technology")]
+    nonfuel_costslearn[technology == "FCEV" & year >= 2020, non_fuel_price := non_fuel_price[year == 2020], by = c("iso", "vehicle_type", "technology")]
+  } else if (rebates_febatesFCEV){
+    ## after 2035, the "original" price of 2020 (before the rebates) is used
+    nonfuel_costslearn[technology == "FCEV", non_fuel_price := ifelse(year > 2035, non_fuel_price[year == 2020], non_fuel_price), by = c("iso", "vehicle_type", "technology")]
+    nonfuel_costslearn[technology == "BEV" & year >= 2020, non_fuel_price := non_fuel_price[year == 2020], by = c("iso", "vehicle_type", "technology")]
   } else {
     ## in case of no rebates, the price of 2020 applies to all time steps
-    nonfuel_costsBEV[year >= 2020, non_fuel_price := non_fuel_price[year == 2020], by = c("iso", "vehicle_type")]
+    nonfuel_costslearn[year >= 2020, non_fuel_price := non_fuel_price[year == 2020], by = c("iso", "vehicle_type", "technology")]
   }
 
-  nonfuel_costsBEV = merge(demand, nonfuel_costsBEV, all.x = TRUE, by = "year")
+  nonfuel_costslearn = merge(demand, nonfuel_costslearn, all.x = TRUE, by = c("year", "technology"))
   ## powertrain represents ~20% of the total purchase price, which represents the ~80% of the non-fuel price
   batterycomponent = 0.2*0.8 ## average number 80% for purchase cost
-  nonfuel_costsBEV[year >= 2020, non_fuel_price := ifelse(!is.na(factor),factor*batterycomponent*non_fuel_price+(1-batterycomponent)*non_fuel_price, non_fuel_price)]
-  nonfuel_costsBEV = nonfuel_costsBEV[year >= 2020]
-  nonfuel_costsBEV[,c("factor", "cumul", "vehicles_number"):= NULL]
-  nonfuel_costs = nonfuel_costs[!(technology=="BEV" & subsector_L1 =="trn_pass_road_LDV_4W" & year >=2020),]
-  nonfuel_costs = rbind(nonfuel_costs, nonfuel_costsBEV)
+  ## powertrain represents ??% of the average purchase price
+  fuelcellcomponent = 0.3 ## average
+  nonfuel_costslearn[year >= 2020 & technology == "BEV", non_fuel_price := ifelse(!is.na(factor),factor*batterycomponent*non_fuel_price+(1-batterycomponent)*non_fuel_price, non_fuel_price)]
+  nonfuel_costslearn[year >= 2020 & technology == "FCEV", non_fuel_price := ifelse(!is.na(factor),factor*fuelcellcomponent*non_fuel_price+(1-fuelcellcomponent)*non_fuel_price, non_fuel_price)]
+  nonfuel_costslearn = nonfuel_costslearn[year >= 2020]
+  nonfuel_costslearn[,c("factor", "cumul", "vehicles_number", "initialcap", "initialyear"):= NULL]
+    ## BEV are too cheap, the models wants them in case of moving to FCEVs: learning has to be switched off for BEVs in Hydrogen push scenarios
+  if (techswitch != "FCEV") {
+    techlearn = c("BEV", "FCEV")
+  } else {
+    techlearn = "FCEV"
+  }
+  nonfuel_costs = nonfuel_costs[!(technology %in% techlearn & subsector_L1 =="trn_pass_road_LDV_4W" & year >=2020),]
+  nonfuel_costs = rbind(nonfuel_costs, nonfuel_costslearn[technology %in% techlearn])
+
 
   return(nonfuel_costs)
 }
-
 
 #' Calculate number of vehicles scaling up the normalized demand with the aggregate total demand
 #'
@@ -75,7 +98,7 @@ calc_num_vehicles_stations <- function(norm_dem, ES_demand_all){
          /annual_mileage]             ## in trillion veh
 
   LDVdem = LDVdem[, .(iso, year, vehicles_number, technology, vehicle_type)]
-  BEVdem = LDVdem[technology == "BEV",][, .(iso, year, vehicles_number, vehicle_type)]
+  learntechdem = LDVdem[technology %in% c("BEV", "FCEV"),][, .(iso, year, vehicles_number, vehicle_type, technology)]
 
   stations = LDVdem[, .(vehicles_number = sum(vehicles_number)), by = c("iso", "year", "technology")]
   stations = stations[year >= 2020]
@@ -93,5 +116,5 @@ calc_num_vehicles_stations <- function(norm_dem, ES_demand_all){
 
   stations = stations[,.(iso, technology, year, fracst)]
 
-  return(list(BEVdem = BEVdem, stations = stations))
+  return(list(learntechdem = learntechdem, stations = stations))
 }
