ec57283e765d2aea9b6b140aebb251060ad56432
Marianna Rottoli <rottoli@pik-potsdam.de>
2020-11-13 12:35:36 +0100

    Added doubling of logit exponent for LDVs and road freight.

--- R/calculate_logit_inconv_endog.R
+++ R/calculate_logit_inconv_endog.R
@@ -140,6 +140,11 @@ calculate_logit_inconv_endog = function(prices,
     ## needs random lambdas for the sectors that are not explicitly calculated
     df <- df[ is.na(logit.exponent), logit.exponent := -10]
 
+    ## logit exponent gets higher in time for LDVs and road freight, doubling by 2035
+    df[subsector_L1 %in% c("trn_freight_road_tmp_subsector_L1", "trn_pass_road_LDV_4W") & year >=2020, logit.exponent := ifelse(year <= 2035 & year >= 2020, logit.exponent[year==2020] + (2*logit.exponent[year==2020]-logit.exponent[year==2020]) * (year-2020)/(2035-2020), 2*logit.exponent[year==2020]),
+       by=c("iso", "vehicle_type", "technology")]
+
+
     ## define the years on which the inconvenience price will be calculated on the basis of the previous time steps sales
     futyears_all = seq(2020, 2101, 1)
     ## all modes other then 4W calculated with exogenous sws
