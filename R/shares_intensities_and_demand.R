75f11271bb0d7fb74220b397284dbebd282dec36
johannah-pik <89136160+johannah-pik@users.noreply.github.com>
2022-05-13 15:51:31 +0200

    Define fuel as variable
    -Remove scetor_fuel at it is no longer used

--- R/shares_intensities_and_demand.R
+++ R/shares_intensities_and_demand.R
@@ -19,7 +19,7 @@ shares_intensity_and_demand <- function(logit_shares,
     `.` <- share <- region <- sector <- subsector_L3 <- subsector_L2 <- subsector_L1 <- NULL
     demand_L2 <- demand_L1 <- demand_L3 <- vehicle_type <- demand_V <- demand_EJ <- demand_F <- NULL
     technology <- MJ_km <- demand_EJel <- demand_EJliq <- MJ_kmel <- MJ_kmliq <- NULL
-    variable <- sector_fuel <- CES_node <- Value_demand <- value <- CES_parent <- NULL
+    variable <- CES_node <- Value_demand <- value <- CES_parent <- fuel <- NULL
     ## load the shares at each level
     S3S_shares <- logit_shares[["S3S_shares"]]
     S2S3_shares <- logit_shares[["S2S3_shares"]]
@@ -52,7 +52,7 @@ shares_intensity_and_demand <- function(logit_shares,
     demand = merge(demand, FV_shares, all=TRUE, by = c("region", "year", "subsector_L1", "vehicle_type"))
     demand = demand[,.(demand_F = demand_V*share, region, sector, year, subsector_L3, subsector_L2, subsector_L1, vehicle_type, technology)]
 
-   
+
     ## put aside the non motorized modes
     demandNM = demand[subsector_L3 %in% c("Cycle", "Walk")]
 
